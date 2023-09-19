b1d2fc6cd3f3ba17f6c80f4c9112bef2d4375afa
JENIGORLA NARESH <nareshjenigorla@gmail.com>
2023-09-19 18:23:47 +0530

    fifth commit

--- src/components/Herosection/Hero.js
+++ src/components/Herosection/Hero.js
@@ -13,25 +13,22 @@ function Herosection() {
         <div className="right col-sm-6 align-items-center justify-content-center d-flex ">
           
           <div className="details align-items-center justify-content-center align-items-center text-center">
-              <h4 className="text-success mb-3"><strong>Hello i am....</strong></h4>
+              <h4 className="text-success mb-3"><strong>Hi all 👋 I'm </strong></h4>
               <h1 className="hero-title"><strong>Jenigorla Naresh</strong></h1>
               <div className="d-inline-block">
                 <ul className="text-center h4 list-unstyled">
                   <li><strong>A Web Developer</strong></li>
-                  <li><strong>A Software Analyst</strong></li>
-                  <li><strong>An FSD Enthusiast</strong></li>
+                  <li><strong>A Big Data Enthusiast</strong></li>
+                  <li><strong>Data Analytics</strong></li>
                 </ul>
               </div>
 
               <p className="text-center mt-5 description" >
-                Thanks for landing on my profile.
-                I'm an ambitious,disciplined and visionary person. 
-                I believe in competing with myself rather than others, 
-                for becoming better than myself. 
-                I like to work with amazing people around the world. New people are new experiences.
+              A passionate Full Stack Software Developer having an experience of building Web
+                applications with JavaScript / Reactjs / Nodejs  and
+                some other cool libraries and frameworks.
               </p>
-
-              {/* <a className="btn btn-success btn-lg border-0 primary-btn-shadow" href="#more" role="button" >Find out more</a> */}
+              <a className="btn btn-success btn-lg border-0 primary-btn-shadow" href="#Contact" role="button" >Contact Me</a>
           </div>
         
         </div>
